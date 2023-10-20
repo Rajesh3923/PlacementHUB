309d44ac35f9d49ea46a13a4541a25621fb9426b
rajesh <pasupuletirajesh460@gmail.com>
2023-10-20 13:03:48 +0530

    added the validation for registration pages and changed the bg of stureg forms

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.models import User,auth
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import csrf_protect
+import openpyxl
 
 
 
